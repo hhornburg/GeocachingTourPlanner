d3f25917a5279f083115b6ae534675b0517fe3d5
pingurus <pingurus@t-online.de>
2018-04-19 14:59:36 +0200

    Fixed bug of failing import of OSM Data

--- UpdateCurrentReleaseFolder.cmd
+++ UpdateCurrentReleaseFolder.cmd
@@ -3,3 +3,4 @@ xcopy "NOTICE" "CurrentRelease\" /y
 xcopy "LICENSE" "CurrentRelease\" /y
 xcopy "bin\Release\*.exe" "CurrentRelease\" /y
 xcopy "bin\Release\*.dll" "CurrentRelease\" /y
+xcopy "bin\Release\*.config" "CurrentRelease\" /y
